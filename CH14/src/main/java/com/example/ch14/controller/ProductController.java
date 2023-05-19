8490fa031eaf020091fd3de8be6f50d3f65e135e
HarenKei <seungmin1266@gmail.com>
2023-05-19 15:25:27 +0900

    Update CH14 : 20230519

---
+++ CH14/src/main/java/com/example/ch14/controller/ProductController.java
@@ -0,0 +1,46 @@
+package com.example.ch14.controller;
+
+import com.example.ch14.dto.ChangeProductDto;
+import com.example.ch14.dto.ProductDto;
+import com.example.ch14.dto.ProductResponseDto;
+import com.example.ch14.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+
+    private final ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping()
+    public ResponseEntity<ProductResponseDto> getProduct(Long number) {
+        ProductResponseDto productResponseDto = productService.getProduct(number);
+        return ResponseEntity.status(HttpStatus.OK).body(productResponseDto);
+    }
+
+    @PostMapping()
+    public ResponseEntity<ProductResponseDto> createProduct(@RequestBody ProductDto productDto) {
+        ProductResponseDto productResponseDto = productService.saveProduct(productDto);
+        return ResponseEntity.status(HttpStatus.OK).body(productResponseDto);
+    }
+
+    @PutMapping()
+    public ResponseEntity<ProductResponseDto> changeProductName(@RequestBody ChangeProductDto changeProductDto) throws Exception{
+        ProductResponseDto productResponseDto = productService.changeProductName(changeProductDto.getNumber(), changeProductDto.getName());
+        return ResponseEntity.status(HttpStatus.OK).body(productResponseDto);
+    }
+
+    @DeleteMapping()
+    public ResponseEntity<String> deleteProduct(Long number) throws Exception{
+        productService.deleteProduct(number);
+        return ResponseEntity.status(HttpStatus.OK).body("정상적으로 삭제되었습니다.");
+    }
+}
